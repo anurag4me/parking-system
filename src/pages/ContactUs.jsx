99e86fd3d481a40758958a5c9f5db1f626f047ee
ChetanAnilMore <chetanmore733@gmail.com>
2024-10-09 10:29:07 +0530

    Changed Button to Submit

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -40,10 +40,7 @@ const ContactUs = () => {
   };
 
   return (
-    <section
-      id="feedback"
-      className="text-black"
-    >
+    <section id="feedback" className="text-black">
       <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
@@ -134,7 +131,7 @@ const ContactUs = () => {
             type="submit"
             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
           >
-            Button
+            Submit
           </button>
         </form>
       </div>
